139458e0af1bd282d931d9c02d81b28cb4cb9cd9
SyedAliZafar <syedalizafar0@gmail.com>
2025-07-04 17:26:21 +0200

    Initial commit with FastAPI and CI pipeline

---
+++ tests/test_health.py
@@ -0,0 +1,10 @@
+# tests/test_health.py
+# check to see if fast server is responding or not
+from fastapi.testclient import TestClient
+from app.main import app
+
+client = TestClient(app)
+
+def test_upload_endpoint_exists():
+    response = client.get("/docs")
+    assert response.status_code == 200
